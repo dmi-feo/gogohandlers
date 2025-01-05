a132ac71f4801571818d42cbaf79739ab314f0ba
Dmitry Fedorov <fdi1992@gmail.com>
2025-01-05 23:45:33 +0100

    more test

--- handler_test.go
+++ handler_test.go
@@ -1,6 +1,7 @@
 package gogohandlers
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
 	"net/http/httptest"
@@ -32,5 +33,49 @@ func TestGGHandler(t *testing.T) {
 		handler.ServeHTTP(response, request)
 
 		require.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("unhandled error", func(t *testing.T) {
+		var handlerFunc = func(ggreq *GGRequest[struct{}, struct{}, struct{}]) (*GGResponse[struct{}, struct{}], error) {
+			return &GGResponse[struct{}, struct{}]{}, fmt.Errorf("this is an error")
+		}
+
+		handler := GGHandler[struct{}, struct{}, struct{}, struct{}, struct{}]{
+			ServiceProvider: &struct{}{},
+			HandlerFunc:     handlerFunc,
+			Middlewares:     []func(hFunc func(*GGRequest[struct{}, struct{}, struct{}]) (*GGResponse[struct{}, struct{}], error)) func(*GGRequest[struct{}, struct{}, struct{}]) (*GGResponse[struct{}, struct{}], error){},
+			Logger:          logger,
+		}
+
+		request, err := http.NewRequest(http.MethodGet, "/", nil)
+		require.NoError(t, err)
+		response := httptest.NewRecorder()
+
+		handler.ServeHTTP(response, request)
+
+		require.Equal(t, http.StatusInternalServerError, response.Code)
+	})
+
+	t.Run("custom response code", func(t *testing.T) {
+		theCode := http.StatusAccepted
+
+		var handlerFunc = func(ggreq *GGRequest[struct{}, struct{}, struct{}]) (*GGResponse[struct{}, struct{}], error) {
+			return &GGResponse[struct{}, struct{}]{StatusCode: theCode}, nil
+		}
+
+		handler := GGHandler[struct{}, struct{}, struct{}, struct{}, struct{}]{
+			ServiceProvider: &struct{}{},
+			HandlerFunc:     handlerFunc,
+			Middlewares:     []func(hFunc func(*GGRequest[struct{}, struct{}, struct{}]) (*GGResponse[struct{}, struct{}], error)) func(*GGRequest[struct{}, struct{}, struct{}]) (*GGResponse[struct{}, struct{}], error){},
+			Logger:          logger,
+		}
+
+		request, err := http.NewRequest(http.MethodGet, "/", nil)
+		require.NoError(t, err)
+		response := httptest.NewRecorder()
+
+		handler.ServeHTTP(response, request)
+
+		require.Equal(t, theCode, response.Code)
 	})
 }
--- mw_data_processing_test.go
+++ mw_data_processing_test.go
@@ -1,6 +1,8 @@
 package gogohandlers
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -43,7 +45,56 @@ func TestDataProcessingMiddleware(t *testing.T) {
 			Logger: logger,
 		}
 
-		request, err := http.NewRequest(http.MethodGet, "/?param_str=hello&param_bool=false", nil)
+		request, err := http.NewRequest(http.MethodGet, "/?param_str=hello&param_bool=0", nil)
+		require.NoError(t, err)
+		response := httptest.NewRecorder()
+
+		handler.ServeHTTP(response, request)
+
+		require.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("request body", func(t *testing.T) {
+		type NestedSchema struct {
+			StringField string `json:"field_str"`
+		}
+		type RequestBodySchema struct {
+			StringField string       `json:"field_str"`
+			IntField    int          `json:"field_int"`
+			FloatField  float64      `json:"field_float"`
+			BoolField   bool         `json:"field_bool"`
+			NestedField NestedSchema `json:"nested_field"`
+		}
+
+		reqBody := RequestBodySchema{
+			StringField: "hello",
+			IntField:    13,
+			FloatField:  0.1,
+			BoolField:   true,
+			NestedField: NestedSchema{
+				StringField: "world",
+			},
+		}
+
+		var handlerFunc = func(ggreq *GGRequest[struct{}, RequestBodySchema, struct{}]) (*GGResponse[struct{}, struct{}], error) {
+			if *ggreq.RequestData != reqBody {
+				return &GGResponse[struct{}, struct{}]{}, fmt.Errorf("request_body has unexpected value: %+v", ggreq.RequestData)
+			}
+			return &GGResponse[struct{}, struct{}]{}, nil
+		}
+
+		handler := GGHandler[struct{}, RequestBodySchema, struct{}, struct{}, struct{}]{
+			ServiceProvider: &struct{}{},
+			HandlerFunc:     handlerFunc,
+			Middlewares: []func(hFunc func(*GGRequest[struct{}, RequestBodySchema, struct{}]) (*GGResponse[struct{}, struct{}], error)) func(*GGRequest[struct{}, RequestBodySchema, struct{}]) (*GGResponse[struct{}, struct{}], error){
+				GetDataProcessingMiddleware[struct{}, RequestBodySchema, struct{}, struct{}, struct{}](nil),
+			},
+			Logger: logger,
+		}
+
+		bodySerialized, err := json.Marshal(reqBody)
+		require.NoError(t, err)
+		request, err := http.NewRequest(http.MethodGet, "/", bytes.NewReader(bodySerialized))
 		require.NoError(t, err)
 		response := httptest.NewRecorder()
 
